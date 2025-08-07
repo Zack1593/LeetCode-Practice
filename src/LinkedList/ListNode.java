7a5223b06763e117f797012158f0915b326ceceb
Zack Gao <zackgaousa@gmail.com>
2025-08-06 21:21:50 -0500

    List node main

---
+++ src/LinkedList/ListNode.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class ListNode {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    // Definition for singly-linked list.
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
